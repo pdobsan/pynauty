f98f34f384c300ac30670b09a3efaeacdcc066df
Andres Goens <goens@goenshome>
2021-02-05 15:12:57 +0100

    converted tests to pytest

--- tests/test_autgrp.py
+++ tests/test_autgrp.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 
 import sys
 from pynauty import autgrp
+import pytest
 
 # List of graphs for testing
 #
@@ -12,28 +13,9 @@ from pynauty import autgrp
 # numorbit, grpsize, generators was calculated by dreadnut
 #
 
-from data_graphs import graphs
-
-
-if __name__ == '__main__':
-    print('Testing pynauty.autgrp()')
-    print('Python version: ' + sys.version)
-    print('Starting ...')
-    passed = 0
-    failed = 0
-    for gname, g, numorbit, grpsize, gens in graphs:
-        print('%-17s ...' % gname, end=' ')
-        sys.stdout.flush()
-        generators, order, o2, orbits, orbit_no = autgrp(g)
-        if generators == gens and orbit_no == numorbit and order == grpsize:
-            print('OK')
-            passed += 1
-        else:
-            print('failed')
-            failed +=1
-    print('... done.')
-    if failed > 0:
-        print('passed = %d   failed = %d' % (passed, failed))
-    else:
-        print('All tests passed.')
-
+def test_autgrp(graph):
+    gname, g, numorbit, grpsize, gens = graph
+    print('%-17s ...' % gname, end=' ')
+    sys.stdout.flush()
+    generators, order, o2, orbits, orbit_no = autgrp(g)
+    assert generators == gens and orbit_no == numorbit and order == grpsize
