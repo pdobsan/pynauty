19875d094132e2f8eed5f8c6cf0fd2c8e6fcdf14
Peter Dobsan <pdobsan@gmail.com>
2021-02-16 22:29:42 +0100

    Added a copy method to Graph.

--- tests/test_isomorphic.py
+++ tests/test_isomorphic.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import copy
 from pynauty import isomorphic, delete_random_edge, Version
 from data_graphs import graphs
 
@@ -9,14 +8,14 @@ from data_graphs import graphs
 if __name__ == '__main__':
     print(Version())
     print('Python version: ' + sys.version)
-    print('Testing pynauty.{isomorphic(),certificate()}')
+    print('Testing pynauty.{isomorphic(),certificate(),delete_random_edge(),copy()}')
     print('Starting ...')
     passed = 0
     failed = 0
     for gname, g, numorbit, grpsize, gens in graphs:
         print('%-37s ...' % gname, end=' ')
         sys.stdout.flush()
-        x =copy.deepcopy(g)
+        x = g.copy()
         if isomorphic(g,x):
             print('OK')
             passed += 1
