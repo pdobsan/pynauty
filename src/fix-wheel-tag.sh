b28ec8e0757908ebf4b0e450cc92f536cdf4a71a
Peter Dobsan <pdobsan@gmail.com>
2021-03-10 20:45:07 +0100

    Fixing wheel tag on linux to ...manylinux_X_Y_...

--- src/fix-wheel-tag.sh
+++ src/fix-wheel-tag.sh
@@ -4,20 +4,14 @@
 #
 
 echo Fixing wheel tag ...
-if type pacman &> /dev/null
-then
-  echo ... on Archlinux ...
-  X=$(pacman -Q glibc | cut -d ' ' -f 2 | cut -d - -f 1 | cut -d . -f 1)
-  Y=$(pacman -Q glibc | cut -d ' ' -f 2 | cut -d - -f 1 | cut -d . -f 2)
+if [ `uname -s` = Linux ]; then
+  echo ... on Linux ...
+  X=$(ldd --version | head -1 | cut -d ' ' -f 4 | cut -d . -f 1)
+  Y=$(ldd --version | head -1 | cut -d ' ' -f 4 | cut -d . -f 2)
   TAG=manylinux\_$X\_$Y
 else
-  if [ `uname -s` = Linux ]; then
-    echo ... on Linux ...
-    TAG=manylinux2014
-  else
-    echo ... not on Linux, bailing out ...
-    exit 0
-  fi
+  echo ... not on Linux, bailing out ...
+  exit 0
 fi
 
 ORIG=$(ls pynauty-*-linux_*.whl)
